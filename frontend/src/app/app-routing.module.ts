1f57ceb12ca24c5ec25dad6c10b2c7ae1084f833
heyimdima <bondardima@icloud.com>
2022-07-03 23:13:00 -0800

    added routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NftPreviewCardComponent } from './components/nft-preview-card/nft-preview-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: NftPreviewCardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
